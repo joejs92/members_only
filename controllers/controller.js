0b7884e87beeff5c17fb8afab9eee6ea74bb4aa8
joejs92 <joejs_92@yahoo.com>
2025-05-18 17:33:30 -0400

    create and render message boxes

--- controllers/controller.js
+++ controllers/controller.js
@@ -1,5 +1,10 @@
+const messages = [
+    {title: "Test", author: "Testy Testerson", messageText: "This is just a test to see if the messages will render."},
+    {title: "Next Message Test", author: "Testicles Gloobiopolis", messageText: "This is a test to see if subsequent messages will render correctly."}
+]
+
 async function homePage(req, res){
-    res.render("index");
+    res.render("index", {messages: messages});
 }
 
 async function signUpForm(req, res){
